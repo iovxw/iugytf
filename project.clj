322b92fdfa3fda4f5d0a76a055879f62cfe521c6
Bluek404 <iovxw@outlook.com>
2016-01-06 19:51:41 +0800

    update config

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.0.0"]
+                 [clj-yaml "0.4.0"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :main ^:skip-aot iugytf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/iugytf/core.clj
+++ src/iugytf/core.clj
@@ -1,25 +1,40 @@
 (ns iugytf.core
-  (:require [iugytf.tgapi :as tgapi])
+  (:require [iugytf.tgapi :as tgapi]
+            [clj-yaml.core :as yaml]
+            [clojure.tools.logging :as log])
   (:gen-class))
 
 (defn updates-seq
   ([bot] (updates-seq bot 0))
   ([bot offset]
    (Thread/sleep 500)
-   (let [updates (tgapi/get-updates bot offset)
+   (let [updates (try (tgapi/get-updates bot offset)
+                      (catch Exception e (log/error e "") []))
          new-offset (-> updates (last) (get :update_id -1) (+ 1))]
      (lazy-cat updates (updates-seq bot new-offset)))))
 
-(defn -main
-  "Nothing ... yet."
-  [& args]
-  (let [bot (tgapi/new-bot "KEYKEYKEY")]
+(defn gen-answer [query results]
+  (map-indexed #(array-map "type" "article"
+                           "id" (format "%s" %1)
+                           "title" %2
+                           "description" (format "预览: %s %s" %2 query)
+                           "message_text" (format "%s %s" %2 query))
+               results))
+
+(defn -main [config-file]
+  (let [config (->> (if config-file
+                      config-file
+                      "config.yaml")
+                    (slurp)
+                    (yaml/parse-string))
+        elist (config :emoticons)
+        bot (tgapi/new-bot (config :key))]
     (loop [updates (updates-seq bot)]
       (println (first updates))
       (when-let [query ((first updates) :inline_query)]
-        (tgapi/answer-inline-query bot (query :id)
-                                   [{"type" "article"
-                                     "id" "12345"
-                                     "title" "o(*￣3￣)o"
-                                     "message_text" "o(*￣3￣)o"}]))
+        (try
+          (tgapi/answer-inline-query bot (query :id)
+                                     ; TODO: 使用 offset 翻页
+                                     (gen-answer (query :query) elist))
+          (catch Exception e (log/error e "")))) ; TODO
       (recur (rest updates)))))
