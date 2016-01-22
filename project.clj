cbf189138b37127da033a3a61c41df3b199df63b
Bluek404 <iovxw@outlook.com>
2016-01-22 14:51:21 +0800

    clojure 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://telegram.me/ywzbot"
   :license {:name "The MIT License (MIT)"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "2.0.0"]
                  [clj-yaml "0.4.0"]
