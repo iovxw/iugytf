4864794df96b4db289703b75db971d652dd56291
iovxw <iovxw@outlook.com>
2016-04-05 01:01:33 +0800

    Short log message when pull updates fail

--- src/iugytf/core.clj
+++ src/iugytf/core.clj
@@ -28,8 +28,11 @@
   ([bot offset]
    (Thread/sleep 500)
    (let [updates (try (tgapi/get-updates bot offset)
-                      (catch Exception e (log/error e "") []))
-         new-offset (-> updates (last) (get :update_id -1) (+ 1))]
+                      (catch Exception e
+                        (log/warnf "Pull updates fail: %s" (.getMessage e)) []))
+         new-offset (if (not= (count updates) 0)
+                      (-> updates (last) (get :update_id) (+ 1))
+                      offset)] ; updates 数量为 0，可能获取失败，使用旧的 offset
      (lazy-cat updates (updates-seq bot new-offset)))))
 
 (defn gen-answer [query results]
